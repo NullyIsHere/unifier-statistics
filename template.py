479e1941aac47c0d99ec305a71c76e9997b5837a
ItsAsheer <140397732+NullyIsHere@users.noreply.github.com>
2024-10-23 15:56:20 +0200

    Update template.py

--- template.py
+++ template.py
@@ -1,6 +1,6 @@
 """
 <one line to give the program's name and a brief idea of what it does.>
-Copyright (C) <year>  <name of author>
+Copyright (C) 2024 ItsAsheer
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published
@@ -18,6 +18,38 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import nextcord # use import nextcord for v1.2
 from nextcord.ext import commands
+import os
+import fcntl
+
+# File to store the call count
+count_file = "call_count.txt"
+
+def record_function_call():
+    # Open the file in read-write mode, or create it if it doesn't exist
+    with open(count_file, 'a+') as f:
+        # Lock the file to prevent race conditions
+        fcntl.flock(f, fcntl.LOCK_EX)
+        
+        # Move to the start of the file to read the count
+        f.seek(0)
+        content = f.read().strip()
+        
+        # If file is empty, initialize count to 0
+        if not content:
+            count = 0
+        else:
+            count = int(content)
+        
+        # Increment the count
+        count += 1
+        
+        # Move back to the start and write the new count
+        f.seek(0)
+        f.write(str(count))
+        f.truncate() # Ensure any leftover data is removed
+        
+        # Unlock the file
+        fcntl.flock(f, fcntl.LOCK_UN)
 
 class Template(commands.Cog):
     """A template cog written for unifier-plugin temmplate repo"""
@@ -28,6 +60,10 @@ class Template(commands.Cog):
     @commands.command()
     async def template(self,ctx):
         await ctx.send('This is a template plugin!')
+        
+    async def process(message):
+        record_function_call()
+        return message
 
 def setup(bot):
     bot.add_cog(Template(bot))
